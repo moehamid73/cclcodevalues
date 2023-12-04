10b715b2116e922812a10781220bbed66922f8df
moehamid73 <34278680+moehamid73@users.noreply.github.com>
2023-12-04 08:18:04 -0600

    fixed bug

--- cclcodevalues/settings.py
+++ cclcodevalues/settings.py
@@ -121,9 +121,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-STATICFILES_DIRS = [
-        os.path.join(STATIC_ROOT, 'static')
-]
 
 try:
     from .local_settings import *
